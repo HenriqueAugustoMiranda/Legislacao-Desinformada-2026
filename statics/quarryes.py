f27b4bcf4b751aebcd9724f88d6a3da517cb92b8
HenriqueAugustoMiranda <rickmteixeira10@gmail.com>
2026-03-29 20:05:38 -0300

    primeiro commit

---
+++ statics/quarryes.py
@@ -0,0 +1,55 @@
+queries = [
+
+    #DESINFORMAÇÃO DIRETA (ECA)
+    "eca protege bandido",
+    "eca ajuda criminoso brasil",
+    "eca falhou brasil",
+    "eca injusto com vítimas",
+    "eca só protege menor criminoso",
+
+    #DESINFORMAÇÃO DIRETA (MISOGINIA)
+    "lei misoginia censura homens",
+    "lei misoginia exagero",
+    "lei misoginia absurdo brasil",
+    "lei misoginia vai prender homens",
+    "lei misoginia é piada",
+
+    #FRASES VIRAIS / CLICKBAIT
+    "não pode mais perguntar se está de tpm",
+    "agora é crime perguntar tpm brasil",
+    "homem não pode mais falar nada brasil",
+    "homens sendo censurados feminismo brasil",
+
+    #REDPILL / INCEL
+    "redpill feminismo brasil verdade",
+    "redpill lei feminicidio brasil crítica",
+    "movimento redpill brasil opinião",
+    "incel feminismo brasil opinião",
+    "feminismo destruiu homens brasil",
+
+    #FEMINICÍDIO DISTORCIDO
+    "lei feminicidio injustiça homens",
+    "homem preso injustamente feminicidio",
+    "lei feminicidio exagero brasil",
+    "feminicidio favorece mulheres",
+
+    #CASOS POLÊMICOS
+    "menor comete crime eca brasil absurdo",
+    "caso real eca injustiça",
+    "crime adolescente brasil revolta",
+    "caso feminicidio polêmico brasil",
+
+    #LEI FELCA (OURO)
+    "lei felca o que é",
+    "lei felca é real",
+    "lei felca protege bandido",
+    "lei felca absurdo",
+    "lei felca verdade ou mentira",
+    "eca virou lei felca",
+
+    #CONFUSÃO / COMPARAÇÃO ERRADA
+    "eca vs lei felca",
+    "lei misoginia tira liberdade homens",
+    "leis feminismo brasil exagero",
+    "brasil leis favorecem mulheres"
+]
